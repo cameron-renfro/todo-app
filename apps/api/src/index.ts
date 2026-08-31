1ce622c1146947e37c2d6539b985dd34beeef201
Cameron Renfro <cameron@cameronclarkrenfro.com>
2026-08-31 18:21:03 -0400

    chore: sets up iniital monorepo

---
+++ apps/api/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3002;
+
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
